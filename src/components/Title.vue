ee4ac6053e480784e2ee451e1b9d5a2cb253d111
Brayen Luhat <bfenomenal@gmail.com>
2024-06-25 14:18:29 +0800

    update : kodam

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1 class="text-4xl font-extrabold text-center text-white mb-4">Cek Kodam Lokal 🐯</h1>
+    <h1 class="text-4xl font-extrabold text-center text-white mb-4 p-4">Cek Kodam Lokal 🐯</h1>
   </div>
 </template>
